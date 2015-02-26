5129b4fcfff420a89091730c6234e1cdcc579bc9
marcqueralt <marcqueralt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 23:03:48 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/demomentsomtres-debug/trunk@1100811 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ demomentsomtres-debug.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+  Plugin Name: DeMomentSomTres Debug
+ */
+
+define(DMS3_DEBUG_TEXT_DOMAIN, 'DMS3-debug');
+
+$dms3Debug = new DeMomentSomTres_Debug();
+
+class DeMomentSomTres_Debug {
+
+    const TEXT_DOMAIN = DMS3_DEBUG_TEXT_DOMAIN;
+    const MENU_SLUG = 'dmst_debug';
+    const OPTIONS = 'dmst_debug_options';
+
+    private $pluginURL;
+    private $pluginPath;
+    private $langDir;
+
+    function __construct() {
+        $this->pluginURL = plugin_dir_url(__FILE__);
+        $this->pluginPath = plugin_dir_path(__FILE__);
+        $this->langDir = dirname(plugin_basename(__FILE__)) . '/languages';
+
+        add_action('plugins_loaded', array(&$this, 'plugin_init'));
+    }
+
+    function plugin_init() {
+        load_plugin_textdomain(DMS3_DEBUG_TEXT_DOMAIN, false, $this->langDir);
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('log_errors', 1);
+        ini_set('error_log', WP_CONTENT_DIR . '/debug.log');
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== DeMomentSomTres DEBUG ===
+Contributors: marcqueralt
+Tags: custom debug, multisite
+Tested up to: 4.0
+Stable tag: trunk
+License: GPLv2 or later
+
+Debug only one of my sites in a multisite network
+
+== Description ==
+
+I needed to debug only one of my sites in a multisite network so I was not allowed to use WP_DEBUG.
+
+So, I implemented the changes that WP_DEBUG does but only for the website it is installed.
+
+== Installation ==
+
+Upload and activate it. Debug messages will start.
+
+= Uninstall =
+
+To stop showing debug messages, you only need to deactivate this plugin.
+
+== Changelog ==
+
+= 1.0 =
+
+* First release
